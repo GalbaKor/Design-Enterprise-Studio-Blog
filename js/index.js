d33f56e0421452161724141cb4aab8b3392d343c
GalbaKor <cameron.tebbenham@outlook.com>
2021-11-01 20:47:16 +0000

    navigation flexbox

--- js/index.js
+++ js/index.js
@@ -14,15 +14,10 @@ function openTab(tabName) {
   }
 }
 
-// function openTab(tabName) {
-//   var i, x;
-//   x = document.getElementsByClassName("topic");
-//   if ((x.getElementById(tabName).style.display = "none")) {
-//     for (i = 0; i < x.length; i++) {
-//       x[i].style.display = "none";
-//     }
-//     document.getElementById(tabName).style.display = "block";
-//   } else {
-//     document.getElementById(tabName).style.display = "none";
-//   }
-// }
+function toggleNav(navName) {
+  if (document.getElementById(navName).style.display === "none") {
+    document.getElementById(navName).style.display = "flex";
+  } else {
+    document.getElementById(navName).style.display = "none";
+  }
+}
